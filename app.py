38b4e8e19395a3b4be18e8985114ad71dfe83552
Adam Pallus <pallusa@gmail.com>
2023-09-28 20:46:34 -0700

    fixed authentication

--- app.py
+++ app.py
@@ -2,31 +2,38 @@ import os
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 
 # Load environment variables
 load_dotenv()
 
 # Set up the Flask app
 app = Flask(__name__)
-app.secret_key = 'some_secret_key'  # for flash messages
+app.secret_key = os.urandom(24)
+sp_oauth = SpotifyOAuth(
+    client_id=os.getenv("SPOTIPY_CLIENT_ID"),
+    client_secret=os.getenv("SPOTIPY_CLIENT_SECRET"),
+    redirect_uri=os.getenv("SPOTIPY_REDIRECT_URI"),
+    scope="playlist-modify-public"
+)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    # If token info isn't in the session, redirect to Spotify authentication
+    if not session.get('token_info'):
+        auth_url = sp_oauth.get_authorize_url()
+        return redirect(auth_url)
+    
+    # If this is a POST request (i.e., form submission)
     if request.method == 'POST':
+        token_info = session.get('token_info', {})
+        sp = spotipy.Spotify(auth=token_info['access_token'])  # Use the token to authenticate
+
         spotify_username = request.form['spotify_username']
         playlist_name = request.form['playlist_name']
         tracks_artists_str = request.form['tracks_artists']
         tracks_artists = eval(tracks_artists_str)
         
-        # Set up authentication
-        sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-            client_id=os.getenv("SPOTIPY_CLIENT_ID"),
-            client_secret=os.getenv("SPOTIPY_CLIENT_SECRET"),
-            redirect_uri=os.getenv("SPOTIPY_REDIRECT_URI"),
-            scope="playlist-modify-public"
-        ))
-
         # Create a new playlist
         playlist = sp.user_playlist_create(user=spotify_username, name=playlist_name)
         playlist_id = playlist['id']
@@ -48,5 +55,12 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/callback')
+def callback():
+    token_info = sp_oauth.get_access_token(request.args['code'])
+    session['token_info'] = token_info
+    return redirect(url_for('index'))
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
